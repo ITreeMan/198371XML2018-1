2bdf036633a1383f07e3a5ed091489edfbc2ed2a
ITreeMan <p_anusorn@kkumail.com>
2018-10-21 22:28:09 +0700

    no message

--- Apply Digital/LAB7/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "C:/Users/ITreeMan/Downloads/Lab6Digi/LAB6Board - Final/LAB7/counte_tb_isim_beh.exe" -prj "C:/Users/ITreeMan/Downloads/Lab6Digi/LAB6Board - Final/LAB7/counte_tb_beh.prj" "work.counte_tb" 
